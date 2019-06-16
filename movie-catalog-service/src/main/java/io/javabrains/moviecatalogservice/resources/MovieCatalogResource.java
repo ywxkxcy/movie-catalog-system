95cf594f8b16e26e2a4c2e14bf1595d3b9a781bb
Chenyang Xu <xuchenyangshaoxing@gmail.com>
2019-06-16 10:59:44 -0700

    first commit

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResource.java
@@ -0,0 +1,52 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import io.javabrains.moviecatalogservice.models.UserRating;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+        // get all rated movie IDs (hard coded ratings)
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
+
+        return ratings.getUserRating().stream().map(rating-> {
+            // For each movie ID,call movie info service and get details
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
+            // Put them all together
+            return new CatalogItem(movie.getName(), "Test", rating.getRating());
+        }).collect(Collectors.toList());
+    }
+}
+/*
+Movie movie = webClientBuilder.build()
+        .get()
+        .uri("http://localhost:8082/movies/" + rating.getMovieId())
+        .retrieve()
+        .bodyToMono(Movie.class) // Mono is asynchronous in reactive to promise there will return a object
+        .block(); // block the execution until bodyToMono finish
+*/
